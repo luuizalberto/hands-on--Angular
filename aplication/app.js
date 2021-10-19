aa09931e870c2e1eea73624ce35b334c2887af48
Luiz Alberto <79115384+luuizalberto@users.noreply.github.com>
2021-10-19 20:01:37 -0300

    Step 2

--- aplication/app.js
+++ aplication/app.js
@@ -1,5 +1,7 @@
 var app = angular.module("myShoppingList", []);
 app.controller("myCtrl", function($scope) {
   $scope.produtos = ["Milk", "Bread", "Cheese", "Outro"];
-  $scope.nomes = ["Julia", "Rafa"];
+  $scope.addItem = function () {
+      $scope.produtos.push($scope.item);
+  }
 });
